3e2b824f7cbc7a0d1a66408f772410609ff39a1e
Buna Isufi <bunaisufi@MacBook-Air.local>
2025-12-04 21:24:40 +0100

    h3

--- src/h3/h3_main.java
+++ src/h3/h3_main.java
@@ -1,5 +1,50 @@
+
 package h3;
+
 
 public class h3_main {
 
+public String name;
+public int gebJahr;
+public int alter;
+
+
+private static final int CURRENT_YEAR = 2025;
+
+
+public h3_main() {
+this.name = "";
+this.gebJahr = 0;
+this.alter = 0;
+}
+
+
+public void setName(String name) {
+this.name = name;
+}
+
+
+public void setGebJahr(int gebJahr) {
+this.gebJahr = gebJahr;
+}
+
+
+public void setAlter() {
+this.alter = CURRENT_YEAR - this.gebJahr;
+}
+
+
+public String getName() {
+return this.name;
+}
+
+
+public int getGebJahr() {
+return this.gebJahr;
+}
+
+
+public int getAlter() {
+return this.alter;
+}
 }
